6acf24af95faa05ce115194d72ea229fc362eeea
Kaio Lima Pimentel <dev.kaiolima.github@gmail.com>
2025-05-04 19:20:11 -0300

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,8 +2,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'https://faculdade-3znz.onrender.com',  // Substitua pela URL base do seu backend
-  baseURL: 'http://localhost:8080', 
+   baseURL: 'https://faculdade-3znz.onrender.com',  // Substitua pela URL base do seu backend
+  //baseURL: 'http://localhost:8080', 
 });
 
 export default api;
